bafe06cea3745145ad716eec83b27fb747a6f3c0
jgoupgoup <jeremyfrom44@gmail.com>
2014-04-29 10:41:17 +0200

    ok

--- Planning/main.h
+++ Planning/main.h
@@ -49,7 +49,6 @@ class TimeSlot;
 #include "beans/timeslot.h"
 #include "beans/course.h"
 #include "beans/planning.h"
-#include "beans/module.h"
 #include "intelligence/intelligence.h"
 #include "intelligence/parameters.h"
 
